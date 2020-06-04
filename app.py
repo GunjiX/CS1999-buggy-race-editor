83451ee327e431fb4bf4b4e75cfd9a2657255fc6
Adrien Duffett <duffett@Adriens-MBP.lan>
2020-06-04 14:28:34 +0100

    Phase 1: Flag color option added, basic validation for number of wheels

--- app.py
+++ app.py
@@ -26,12 +26,19 @@ def create_buggy():
     return render_template("buggy-form.html")
   elif request.method == 'POST':
     msg=""
+    qty_wheels = request.form['qty_wheels']
+    if not qty_wheels.isdigit():
+      msg = f"This is not a number: {qty_wheels}"
+      return render_template("buggy-form.html", msg = msg)
     try:
-      qty_wheels = request.form['qty_wheels']
-      msg = f"qty_wheels={qty_wheels}" 
+      flag_color = request.form['flag_color']
+      #msg = f"qty_wheels={qty_wheels}, hamster_booster={hamster_booster}" 
       with sql.connect(DATABASE_FILE) as con:
         cur = con.cursor()
-        cur.execute("UPDATE buggies set qty_wheels=? WHERE id=?", (qty_wheels, DEFAULT_BUGGY_ID))
+        cur.execute(
+          "UPDATE buggies set qty_wheels=?, flag_color=? WHERE id=?", 
+          (qty_wheels, flag_color, DEFAULT_BUGGY_ID)
+        )
         con.commit()
         msg = "Record successfully saved"
     except:
